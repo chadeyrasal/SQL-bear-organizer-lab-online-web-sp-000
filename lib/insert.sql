d9e6e0e4ef29b04452d304f4c47e55512e7a566a
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-07 18:11:40 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,8 +4,13 @@ insert into bears (name, age, gender, color, temperament, alive)
   values ('Rowdy', 14, );
 insert into bears (name, age, gender, color, temperament, alive)
   values ('Tabitha', 3, );
-insert into bears (name, age, gender, color, temperament, alive) values ('Sergeant Brown', 6, )
-insert into bears (name, age, gender, color, temperament, alive) values ('Melissa', 26, )
-insert into bears (name, age, gender, color, temperament, alive) values ('Grinch', 2, )
-insert into bears (name, age, gender, color, temperament, alive) values ('Wendy', 13, )
-insert into bears (name, age, gender, color, temperament, alive) values (null, 18, )
+insert into bears (name, age, gender, color, temperament, alive)
+  values ('Sergeant Brown', 6, );
+insert into bears (name, age, gender, color, temperament, alive)
+  values ('Melissa', 26, );
+insert into bears (name, age, gender, color, temperament, alive)
+  values ('Grinch', 2, );
+insert into bears (name, age, gender, color, temperament, alive)
+  values ('Wendy', 13, );
+insert into bears (name, age, gender, color, temperament, alive)
+  values (null, 18, );
