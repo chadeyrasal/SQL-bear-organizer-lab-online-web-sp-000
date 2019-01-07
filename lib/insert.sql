2bbd82ddf003622aac4cabf075cb9741b0df715b
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-07 18:13:56 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
 insert into bears (name, age, gender, color, temperament, alive)
-  values ('Mr. Chocolate', 22, 'female', 'brown');
+  values ('Mr. Chocolate', 22, 'female', 'brown', 'shy');
 insert into bears (name, age, gender, color, temperament, alive)
-  values ('Rowdy', 14, 'female', 'ginger');
+  values ('Rowdy', 14, 'female', 'ginger', 'shy');
 insert into bears (name, age, gender, color, temperament, alive)
-  values ('Tabitha', 3, 'male', 'ginger');
+  values ('Tabitha', 3, 'male', 'ginger', 'curious');
 insert into bears (name, age, gender, color, temperament, alive)
-  values ('Sergeant Brown', 6, 'male', 'honey');
+  values ('Sergeant Brown', 6, 'male', 'honey', 'curious');
 insert into bears (name, age, gender, color, temperament, alive)
-  values ('Melissa', 26, 'female', 'chestnut');
+  values ('Melissa', 26, 'female', 'chestnut', 'playful');
 insert into bears (name, age, gender, color, temperament, alive)
-  values ('Grinch', 2, 'male', 'honey');
+  values ('Grinch', 2, 'male', 'honey', 'shy');
 insert into bears (name, age, gender, color, temperament, alive)
-  values ('Wendy', 13, 'female', 'chestnut');
+  values ('Wendy', 13, 'female', 'chestnut', 'playful');
 insert into bears (name, age, gender, color, temperament, alive)
-  values (null, 18, 'male', 'brown');
+  values (null, 18, 'male', 'brown', 'angry');
