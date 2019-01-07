a134533f8ca4ea49fbd7f31195b2ed99cc460c99
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-07 18:08:00 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,10 @@
 insert into bears (name, age, gender, color, temperament, alive) values ()
+insert into bears (name, age, gender, color, temperament, alive) values ()
+insert into bears (name, age, gender, color, temperament, alive) values ()
+insert into bears (name, age, gender, color, temperament, alive) values ()
+insert into bears (name, age, gender, color, temperament, alive) values ()
+insert into bears (name, age, gender, color, temperament, alive) values ()
+insert into bears (name, age, gender, color, temperament, alive) values ()
+insert into bears (name, age, gender, color, temperament, alive) values ()
+
+  Mr. Chocolate
